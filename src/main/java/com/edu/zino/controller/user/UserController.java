27e2fb27feb6d87d9aaa0fbaf0841143c6734864
doky18 <dokky126@gmail.com>
2023-03-30 04:36:58 +0900

    loginView.toTeacher-ky

--- src/main/java/com/edu/zino/controller/user/UserController.java
+++ src/main/java/com/edu/zino/controller/user/UserController.java
@@ -2,15 +2,23 @@ package com.edu.zino.controller.user;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.edu.zino.model.member.MemberService;
 
 @Controller
 public class UserController {
 	
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
 	
+	@Autowired
+	private MemberService memberService;
+		
 	@GetMapping("/")
 	public ModelAndView getIndex(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView("index");
@@ -34,5 +42,14 @@ public class UserController {
 		ModelAndView mav = new ModelAndView("/user/message");
 		return mav;
 	}
+	
+	
+	//회원 마이페이지(상세보기)
+	@GetMapping("/user/mypage")
+	public ModelAndView getMyaccount(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("/user/mypage/account");
+		return mav;
+	}
+
 	
 }
